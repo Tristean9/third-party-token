2475473ee5d044587df13ab282b0ae4384cf1fd9
tristean9 <1367927606@qq.com>
2025-01-26 14:35:14 +0800

    模拟第三方登录

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,94 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import {useRouter, useSearchParams} from 'next/navigation';
+import React, {useState, useEffect, Suspense} from 'react';
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const LoginForm: React.FC = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const [ID, setID] = useState('admin');
+    const [password, setPassword] = useState('');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const [ref, setRef] = useState('');
+
+    useEffect(() => {
+        setRef(searchParams.get('ref') || '');
+    }, []);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        const validRoles = ['user', 'guard', 'admin'];
+        if (!validRoles.includes(ID)) {
+            alert('角色不符合要求，请在user, guard, admin中进行选择');
+            return;
+        }
+        router.push(`${ref}?patharmonyRole=${ID}` || 'http://localhost:3000/login');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2 className="text-2xl font-bold mb-6 text-center">
+                用户登录
+            </h2>
+            <div className="mb-4">
+                <label htmlFor="email" className="block text-gray-700">
+                    账号
+                </label>
+                <input
+                    type="text"
+                    id="ID"
+                    value={ID}
+                    onChange={e => setID(e.target.value)}
+                    className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+                    required
+                />
+            </div>
+            <div className="mb-6">
+                <label htmlFor="password" className="block text-gray-700">
+                    密码
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    className="w-full px-3 py-2 border rounded focus:outline-none focus:ring focus:border-blue-300"
+                    // required
+                />
+            </div>
+            <button
+                type="submit"
+                className="w-full bg-customRed text-black py-2 rounded"
+            >
+                登录
+            </button>
+        </form>
+    );
+};
+
+const Landing: React.FC = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white p-6 rounded shadow-md w-full max-w-sm flex flex-col gap-10">
+                <div>
+                    <Image
+                        src="/images/pkulogo_red.svg"
+                        alt="logo"
+                        className="mx-auto mt-20"
+                        width={100}
+                        height={100}
+                    />
+                </div>
+                <LoginForm />
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
+};
+
+export default function LandingPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Landing />
+        </Suspense>
+    );
 }
